1930fa4d0ef6e23bf598622f055760b86de0d396
OIC_Developer <OIC_Developer@oracle.com>
2026-03-10 21:32:55 -0600

    API client checkin

---
+++ terraform-configuration/TF_ROLLBACK_USECASE-2.6.20260311-033252/oic-connections.tf
@@ -0,0 +1,71 @@
+
+# RESTINVOKE
+resource "oic_oicconnection" "restinvoke" {
+  connection_id = "RESTINVOKE"
+  project_id    = oic_oicproject.tf_rollback_usecase_deploymentv1.code
+  name            = "RestInvoke"
+  security_policy = "BASIC_AUTH"
+  role            = "SOURCE_AND_TARGET"
+  scope           = "PRIVATE"
+
+  adapter {
+    name = "rest"
+  }
+
+  connection_properties {
+    property_name  = "connectionType"
+    property_value = "restUrl"
+  }
+
+  connection_properties {
+    property_name  = "connectionUrl"
+    property_value = var.connections["restinvoke"]["connectionurl"]
+  }
+
+
+  security_properties {
+    property_name  = "username"
+    property_value = var.connections["restinvoke"]["sec_username"]
+  }
+
+  security_properties {
+    property_name  = "password"
+    property_value = var.connections["restinvoke"]["sec_password"]
+  }
+
+
+  depends_on = [oic_oicproject.tf_rollback_usecase_deploymentv1, oic_oicproject.tf_rollback_usecase_deploymentv2]
+
+}
+
+
+# WEATHERCONNECTION
+resource "oic_oicconnection" "weatherconnection" {
+  connection_id = "WEATHERCONNECTION"
+  project_id    = oic_oicproject.tf_rollback_usecase_deploymentv1.code
+  name            = "WeatherConnection"
+  security_policy = "NONE"
+  role            = "SOURCE_AND_TARGET"
+  scope           = "PRIVATE"
+
+  adapter {
+    name = "rest"
+  }
+
+  connection_properties {
+    property_name  = "connectionType"
+    property_value = "restUrl"
+  }
+
+  connection_properties {
+    property_name  = "connectionUrl"
+    property_value = var.connections["weatherconnection"]["connectionurl"]
+  }
+
+
+
+  depends_on = [oic_oicproject.tf_rollback_usecase_deploymentv2]
+
+}
+
+
